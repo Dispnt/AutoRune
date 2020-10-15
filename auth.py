10c2bdcb26e24d730fa8410a30ced7aa52143235
dispnt <yuyuanjiajian@gmail.com>
2020-10-15 18:46:48 +0800

    Initial commit

---
+++ auth.py
@@ -0,0 +1,19 @@
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+game_path = 'E:\LeagueofLegends\lockfile'
+try:
+    server_info = open(game_path, "r").read().split(':')
+except IOError:
+    sys.exit('Launch Your Game First')
+else:
+    server_port = server_info[2]
+    server_pwd = server_info[3]
+    server_protocol = server_info[4]
+    #ProcessName:PID:WebServerPort:WebServerPwd:Protocol
+    server_url = f"{server_protocol}://127.0.0.1:{server_port}"
+    print(server_url,server_port,server_pwd)
+
+summoner_info = requests.get(server_url+"/lol-summoner/v1/current-summoner", auth=HTTPBasicAuth('riot', server_pwd), verify=False).json()
+print(summoner_info['summonerId'])
