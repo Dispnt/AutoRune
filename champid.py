ee4852e5156245e0853c2c96ab6897fb1774225c
dispnt <yuyuanjiajian@gmail.com>
2020-10-20 18:38:47 +0800

    Enhanced readability

--- champid.py
+++ champid.py
@@ -1,11 +1,21 @@
 import json
-
 import requests
 
-champList = requests.post("https://game.gtimg.cn/images/lol/act/img/js/heroList/hero_list.js").json()
-champidList = {}
-for champ in champList['hero']:
-    champidList[champ["heroId"]]=champ["alias"]
-json.dump(champidList, open("./champid",'w'))
-d2 = json.load(open('./champid','r'))
-print(d2['2'])
+
+
+
+def champIDInit():
+    champ_list = requests.post("https://game.gtimg.cn/images/lol/act/img/js/heroList/hero_list.js").json()
+    champ_id_list = {}
+    for champ in champ_list['hero']:
+        champ_id_list[champ["heroId"]] = champ["alias"]
+    json.dump(champ_id_list, open("./champid", 'w'))
+
+
+def getChampName(champ_id):
+    d2 = json.load(open('./champid', 'r'))
+    print(d2[champ_id])
+    return d2[champ_id]
+
+# champIDInit()
+# getChampName('2')
