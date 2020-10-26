24d5dccc6a0667ed8cad61ea5c57dc3d343ec6bf
dispnt <yuyuanjiajian@gmail.com>
2020-10-26 19:32:16 +0800

    integration function

--- rune.py
+++ rune.py
@@ -1,77 +1,106 @@
-import json
 import sys
-from requests.auth import HTTPBasicAuth
-import requests
+from time import sleep
 
+from champid import *
+from requests.auth import HTTPBasicAuth
+
+
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 resove_8400 = ["8437", "8439", "8465",
-             "8446", "8463", "8401",
-             "8429", "8444", "8473",
-             "8451", "8453", "8242"]
+               "8446", "8463", "8401",
+               "8429", "8444", "8473",
+               "8451", "8453", "8242"]
 
 inspiration_8300 = ["8351", "8360", "8358",
-                  "8306", "8304", "8313",
-                  "8321", "8316", "8345",
-                  "8347", "8410", "8352"]
+                    "8306", "8304", "8313",
+                    "8321", "8316", "8345",
+                    "8347", "8410", "8352"]
 
 sorcery_8200 = ["8214", "8229", "8230",
-              "8224", "8226", "8275",
-              "8210", "8234", "8233",
-              "8237", "8232", "8236"]
+                "8224", "8226", "8275",
+                "8210", "8234", "8233",
+                "8237", "8232", "8236"]
 
 domination_8100 = ["8112", "8124", "8128", "9923",
-                 "8126", "8139", "8143",
-                 "8136", "8120", "8138",
-                 "8135", "8134", "8105", "8106"]
+                   "8126", "8139", "8143",
+                   "8136", "8120", "8138",
+                   "8135", "8134", "8105", "8106"]
 
 precision_8000 = ["9101", "9111", "8009",
-                "9104", "9105", "9103",
-                "8014", "8017", "8299"]
+                  "9104", "9105", "9103",
+                  "8014", "8017", "8299"]
 
-rune_listname = ['resove_8400','inspiration_8300','sorcery_8200','domination_8100','precision_8000']
+rune_listname = ['resove_8400', 'inspiration_8300', 'sorcery_8200', 'domination_8100', 'precision_8000']
 
 game_path = 'E:\\IdiotGame\\LeagueofLegends\\英雄联盟\\LeagueClient'
-def auth():
-    try:
-        server_info = open(game_path + "\\lockfile", "r").read().split(':')
-    except IOError:
-        sys.exit('Launch Your Game First')
-    else:
-        server_port = server_info[2]
-        server_pwd = server_info[3]
-        server_protocol = server_info[4]
-        # ProcessName:PID:WebServerPort:WebServerPwd:Protocol
-        server_url = f"{server_protocol}://127.0.0.1:{server_port}"
-        print(server_url, server_port, server_pwd)
 
-    summoner_info = requests.get(server_url + "/lol-summoner/v1/current-summoner", auth=HTTPBasicAuth('riot', server_pwd),
-                                 verify=False).json()
-    print(summoner_info['summonerId'])
 
-def runeIDs(championName ='Fizz'):
-    summoner_info = requests.get("http://opgg.dispnt.com/api?championName="+championName).json()
+try:
+    server_info = open(game_path + "\\lockfile", "r").read().split(':')
+except IOError:
+    sys.exit('Launch Your Game First')
+else:
+    global server_url, server_pwd
+    server_port = server_info[2]
+    server_pwd = server_info[3]
+    server_protocol = server_info[4]
+    # ProcessName:PID:WebServerPort:WebServerPwd:Protocol
+    server_url = f"{server_protocol}://127.0.0.1:{server_port}"
+
+    print(server_url, server_port, server_pwd)
+
+# summoner_info = requests.get(server_url + "/lol-summoner/v1/current-summoner",
+#                              auth=HTTPBasicAuth('riot', server_pwd),
+#                              verify=False).json()
+# print(summoner_info['summonerId'])
+
+
+
+def runeIDs(championName):
+    summoner_info = requests.get("http://opgg.dispnt.com/api?championName=" + championName).json()
     selectedId = summoner_info[1]['1']
-    print(selectedPerkId)
+    print(selectedId)
     for list_name in rune_listname:
-        if summoner_info[1]['1'][1] in globals()[list_name]:
-            print("主系ID:"+list_name.split('_')[1])
+        if selectedId[1] in globals()[list_name]:
+            print("主系ID:" + list_name.split('_')[1])
             primaryId = list_name.split('_')[1]
-        if summoner_info[1]['1'][4] in globals()[list_name]:
-            print("副系ID:"+list_name.split('_')[1])
+        if selectedId[4] in globals()[list_name]:
+            print("副系ID:" + list_name.split('_')[1])
             subId = list_name.split('_')[1]
-    return selectedId,subId,primaryId
+    return selectedId, subId, primaryId
 
 
-python2json = {}
-selectedPerkIdSecondPart = ['5008','5008','5003']
-(selectedPerkId,primaryStyleId,subStyleId) = runeIDs('Fizz')
-selectedPerkId.extend(selectedPerkIdSecondPart)
-print(selectedPerkId)
-python2json['current'] = True
-python2json["name"] = "Test"
-python2json["primaryStyleId"] = primaryStyleId
-python2json["selectedPerkIds"] = selectedPerkId
-python2json["subStyleId"] = subStyleId
+def runeJson(championName='Fizz'):
+    rune_str = {}
+    selectedPerkIdSecondPart = ['5008', '5008', '5003']
+    (selectedPerkId, subStyleId, primaryStyleId) = runeIDs(championName)
+    selectedPerkId.extend(selectedPerkIdSecondPart)
+    rune_str['current'] = True
+    rune_str["name"] = "Test"
+    rune_str["primaryStyleId"] = primaryStyleId
+    rune_str["selectedPerkIds"] = selectedPerkId
+    rune_str["subStyleId"] = subStyleId
+    rune_json = json.dumps(rune_str)
+    return rune_json
 
-json_str = json.dumps(python2json)
-print(json_str)
+
+def champSelect():
+    try:
+        champ_select_info = requests.get(server_url + "/lol-champ-select/v1/session",
+                                         auth=HTTPBasicAuth('riot', server_pwd),
+                                         verify=False).json()
+        champ_id = champ_select_info['actions'][0][0]['championId']
+        print(getChampName(str(champ_id)))
+    except Exception as e:
+         print('Waiting...')
+
+
+while True:
+    champSelect()
+    sleep(0.5)
+
+#
+# if auth():
+#     print(runeJson('Fizz'))
