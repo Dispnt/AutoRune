4d571148c2c6c06021339d06e3b645608d7425e2
dispnt <yuyuanjiajian@gmail.com>
2020-11-02 12:57:01 +0800

    display auto picked rune name

--- rune.py
+++ rune.py
@@ -1,38 +1,38 @@
 import sys
-from time import sleep
-
 from champid import *
+from time import sleep
 from requests.auth import HTTPBasicAuth
 
 import urllib3
-
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-resove_8400 = ["8437", "8439", "8465",
+
+
+resove_8400_坚决 = ["8437", "8439", "8465",
                "8446", "8463", "8401",
                "8429", "8444", "8473",
                "8451", "8453", "8242"]
 
-inspiration_8300 = ["8351", "8360", "8358",
+inspiration_8300_启迪 = ["8351", "8360", "8358",
                     "8306", "8304", "8313",
                     "8321", "8316", "8345",
                     "8347", "8410", "8352"]
 
-sorcery_8200 = ["8214", "8229", "8230",
+sorcery_8200_巫术 = ["8214", "8229", "8230",
                 "8224", "8226", "8275",
                 "8210", "8234", "8233",
                 "8237", "8232", "8236"]
 
-domination_8100 = ["8112", "8124", "8128", "9923",
+domination_8100_主宰 = ["8112", "8124", "8128", "9923",
                    "8126", "8139", "8143",
                    "8136", "8120", "8138",
                    "8135", "8134", "8105", "8106"]
 
-precision_8000 = ["9101", "9111", "8009",
+precision_8000_精密 = ["9101", "9111", "8009",
                   "9104", "9105", "9103",
                   "8014", "8017", "8299"]
 
-rune_listname = ['resove_8400', 'inspiration_8300', 'sorcery_8200', 'domination_8100', 'precision_8000']
+rune_listname = ['resove_8400_坚决', 'inspiration_8300_启迪', 'sorcery_8200_巫术', 'domination_8100_主宰', 'precision_8000_精密']
 
 game_path = 'E:\\IdiotGame\\LeagueofLegends\\英雄联盟\\LeagueClient'
 
@@ -60,13 +60,18 @@ else:
 def runeIDs(championName):
     summoner_info = requests.get("http://opgg.dispnt.com/api?championName=" + championName).json()
     selectedId = summoner_info[1]['1']
-    print(summoner_info[0]['1'])
+    for rune in summoner_info[0]['1']:
+        print(rune)
     for list_name in rune_listname:
         if selectedId[1] in globals()[list_name]:
-            print("主系ID:" + list_name.split('_')[1])
+            print("主系:" + list_name.split('_')[2])
+            for paimary_rune in summoner_info[0]['1'][:4]:
+                print(paimary_rune)
             primaryId = list_name.split('_')[1]
         if selectedId[4] in globals()[list_name]:
-            print("副系ID:" + list_name.split('_')[1])
+            print("副系:" + list_name.split('_')[2])
+            for sub_rune in summoner_info[0]['1'][2:]:
+                print(rune)
             subId = list_name.split('_')[1]
     return selectedId, subId, primaryId
 
