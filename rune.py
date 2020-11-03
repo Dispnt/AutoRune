b66c501b27aca589a0f01fa977f90678bc559dd7
dispnt <yuyuanjiajian@gmail.com>
2020-11-03 17:32:38 +0800

    display champion name and rune color

--- rune.py
+++ rune.py
@@ -4,33 +4,42 @@ from time import sleep
 from requests.auth import HTTPBasicAuth
 
 import urllib3
+
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
+class RuneColor:
+    resove = '\033[32m'
+    inspiration = '\033[34m'
+    sorcery = '\033[35m'
+    domination = '\033[31m'
+    precision = '\033[33m'
+    WARNING = '\033[93m'
+
 
 resove_8400_坚决 = ["8437", "8439", "8465",
-               "8446", "8463", "8401",
-               "8429", "8444", "8473",
-               "8451", "8453", "8242"]
+                  "8446", "8463", "8401",
+                  "8429", "8444", "8473",
+                  "8451", "8453", "8242"]
 
 inspiration_8300_启迪 = ["8351", "8360", "8358",
-                    "8306", "8304", "8313",
-                    "8321", "8316", "8345",
-                    "8347", "8410", "8352"]
+                       "8306", "8304", "8313",
+                       "8321", "8316", "8345",
+                       "8347", "8410", "8352"]
 
 sorcery_8200_巫术 = ["8214", "8229", "8230",
-                "8224", "8226", "8275",
-                "8210", "8234", "8233",
-                "8237", "8232", "8236"]
+                   "8224", "8226", "8275",
+                   "8210", "8234", "8233",
+                   "8237", "8232", "8236"]
 
 domination_8100_主宰 = ["8112", "8124", "8128", "9923",
-                   "8126", "8139", "8143",
-                   "8136", "8120", "8138",
-                   "8135", "8134", "8105", "8106"]
+                      "8126", "8139", "8143",
+                      "8136", "8120", "8138",
+                      "8135", "8134", "8105", "8106"]
 
 precision_8000_精密 = ["9101", "9111", "8009",
-                  "9104", "9105", "9103",
-                  "8014", "8017", "8299"]
+                     "9104", "9105", "9103",
+                     "8014", "8017", "8299"]
 
 rune_listname = ['resove_8400_坚决', 'inspiration_8300_启迪', 'sorcery_8200_巫术', 'domination_8100_主宰', 'precision_8000_精密']
 
@@ -59,18 +68,18 @@ else:
 def runeIDs(championName):
     summoner_info = requests.get("http://opgg.dispnt.com/api?championName=" + championName).json()
     selectedId = summoner_info[1]['1']
-    for rune in summoner_info[0]['1']:
-        print(rune)
+
+    print(f"{championName} 的 符文")
     for list_name in rune_listname:
         if selectedId[1] in globals()[list_name]:
-            print(f"{getattr(runeColor, list_name.split('_')[0])}---主系---{list_name.split('_')[2]} ")
+            print(f"{getattr(RuneColor, list_name.split('_')[0])}---主系---  {list_name.split('_')[2]} ")
             for paimary_rune in summoner_info[0]['1'][:4]:
-                print(f"{getattr(runeColor, list_name.split('_')[0])}{paimary_rune}", end="\t")
+                print(f"{getattr(RuneColor, list_name.split('_')[0])}{paimary_rune}", end="  ")
             primaryId = list_name.split('_')[1]
         if selectedId[4] in globals()[list_name]:
-            print(f"\n{getattr(runeColor, list_name.split('_')[0])}---副系---{list_name.split('_')[2]}")
+            print(f"\n{getattr(RuneColor, list_name.split('_')[0])}---副系---  {list_name.split('_')[2]}")
             for sub_rune in summoner_info[0]['1'][4:]:
-                print(f"{getattr(runeColor, list_name.split('_')[0])}{sub_rune}", end="\t")
+                print(f"{getattr(RuneColor, list_name.split('_')[0])}{sub_rune}", end="  ")
             subId = list_name.split('_')[1]
     return selectedId, subId, primaryId
 
